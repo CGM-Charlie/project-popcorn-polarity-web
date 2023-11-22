b85cb3fe98053faefcf439dd7a13dfa3d8cb40c5
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2023-11-22 15:33:38 -0600

    Add simple landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,22 @@
-import { Flex, Spacer, Text } from "@chakra-ui/react";
+import { Flex, Spacer, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
 function LandingPage() { 
     return(
-        <Flex width="100%" direction='column' align='center'>
-            <Spacer></Spacer>
-            <Text>Landing Page</Text>
-            <Spacer></Spacer>
+        <Flex height="500px" width="100%" direction='column' alignItems='center'>
+            <Spacer />
+            <VStack>
+                <Text 
+                    as='b' 
+                    fontSize='6xl'
+                    bgGradient='linear(to-l, #36D1DC, #5B86E5)'
+                    bgClip='text'
+                >
+                    Discover the Emotion Behind Every Review
+                </Text>
+                <Text fontSize='2xl'>Leveraging DistilBERT for Advanced Sentiment Analysis</Text>
+            </VStack>
+            <Spacer />
         </Flex>
     )
 }
